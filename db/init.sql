ed3465629651e3f3b320b5e257bbf5ddd4b3c886
Peter <thiessenp@gmail.com>
2020-10-15 21:27:48 -0400

    Updates SQL with roster and transcript views

--- db/init.sql
+++ db/init.sql
@@ -1,16 +1,21 @@
+--
 -- Creates inital POSTGRESQL ChatApp-Web tables to store messages etc.
 --
 -- Tests:
 -- copy paste from `test.sql` in console, success if SQL results
 --
 
+--
 -- For UUID ids
 -- UUIDs add security since difficult to guess and gaurantee uniqueness. If the
 -- data is not mutable, then a semantic key, like a column with names is fine to.
 -- NOTE: UUIDs are 128bit HEX digits, not strings
+--
 CREATE EXTENSION "uuid-ossp";
 
 
+DROP VIEW IF EXISTS roster;
+DROP VIEW IF EXISTS transcript;
 DROP TABLE IF EXISTS chat_message;
 DROP TABLE IF EXISTS chat_user;
 DROP TABLE IF EXISTS chat;
@@ -22,13 +27,17 @@ CREATE TABLE account (
     is_authenticated BOOLEAN DEFAULT FALSE
 );
 
+--
 -- Parent, no knowledge of children (chat_message & chat_user)
+--
 CREATE TABLE chat (
     id uuid PRIMARY KEY default uuid_generate_v4(),
     name VARCHAR (50)
 );
 
+--
 -- Child, knows about parent Chat & IS-A type of Account
+--
 CREATE TABLE chat_user (
     id uuid PRIMARY KEY default uuid_generate_v4(), -- Why? id & account_id? alows chat_user to exist in mulitple chats
     account_id uuid,
@@ -38,15 +47,20 @@ CREATE TABLE chat_user (
     CONSTRAINT fk_chat_id FOREIGN KEY(chat_id) REFERENCES chat(id)
 );
 
+--
 -- Child, knows about parent
 -- note 1-TO-1 rel w/ chat_user
+-- note IMPORTANT if a message is not to anyone, then put TO SELF. This is a workaround/hack 
+-- to work with inner join. W// to self, any message w//to_chat_user_id would be
+-- excluded. A Left Outer Join would work but include all messages..
+--
 CREATE TABLE chat_message (
     id uuid PRIMARY KEY default uuid_generate_v4(),
     message_id SERIAL,  -- note separate message_id for Client to have ordered message sequence
     chat_id uuid,
     timestamp timestamp DEFAULT now(), 
     from_chat_user_id uuid NOT NULL,
-    to_chat_user_id uuid,   -- rare case TO a user but is a nice feature
+    to_chat_user_id uuid NOT NULL,   -- NOT To anyone? Then put TO SELF (messy?) DOCUMENT THIS IN API!
     content VARCHAR (500) NOT NULL,
     attachments JSON, 
     CONSTRAINT fk_chat_id FOREIGN KEY(chat_id) REFERENCES chat(id),
@@ -54,6 +68,28 @@ CREATE TABLE chat_message (
     CONSTRAINT fk_to_chat_user_id FOREIGN KEY(to_chat_user_id) REFERENCES chat_user(id)
 );
 
+--
+-- Roster: Create View for API convenience
+--
+CREATE OR REPLACE VIEW roster AS 
+SELECT chat_user.id, chat_id, username, is_authenticated, is_typing FROM chat_user 
+INNER JOIN account ON chat_user.account_id = account.id;
+
+--
+-- Transcript: Create View for API convenience
+-- note had to use aliases with inner join, is this messy?
+--
+CREATE OR REPLACE VIEW transcript as 
+SELECT chat_message.id, message_id, chat_message.chat_id, timestamp, from_chat_user_id, account_from.username AS from_chat_user_username, to_chat_user_id, account_to.username AS to_chat_user_username, content, attachments 
+FROM chat_message 
+INNER JOIN chat_user chat_user_from ON chat_message.from_chat_user_id = chat_user_from.id 
+INNER JOIN account account_from ON chat_user_from.account_id = account_from.id 
+INNER JOIN chat_user chat_user_to ON chat_message.to_chat_user_id = chat_user_to.id 
+INNER JOIN account account_to ON chat_user_to.account_id = account_to.id;
+
+
+--
 -- Used in message lists to create an order in a message list, uuid gives uniqueness not order
 -- SQL starts row indexing at 1, this starts at 0 to match Array indexes starting at 0
+--
 CREATE SEQUENCE message_id_sequence INCREMENT 1 START 0 MINVALUE 0 OWNED BY chat_message.message_id;
--- db/test.sql
+++ db/test.sql
@@ -6,8 +6,10 @@
 -- all good, if not, this failed.
 --
 
+--
 -- Insert Some data
 -- note uuid's copy pasted from past examples
+--
 
 -- Accounts x3
 INSERT INTO account (id, username) VALUES ('a2d45b9b-e6ee-4b2c-ac0a-ef2434890ad5', 'username1');
@@ -23,8 +25,8 @@ INSERT INTO chat_user (id, account_id, chat_id) VALUES ('791be344-1acb-4d15-9284
 INSERT INTO chat_message (chat_id, from_chat_user_id, to_chat_user_id, content) VALUES('f729dd6c-a130-4725-9d98-10137727a821', 'b84c84c6-7a27-42b7-95c4-745ecbefc3d3', '0ba9b218-f4b2-4c81-b10e-df2a5ff681d0', 'content - message from username1 to username2 (chat1)');
 -- message from username2 to username1
 INSERT INTO chat_message (chat_id, from_chat_user_id, to_chat_user_id, content) VALUES('f729dd6c-a130-4725-9d98-10137727a821', '0ba9b218-f4b2-4c81-b10e-df2a5ff681d0', 'b84c84c6-7a27-42b7-95c4-745ecbefc3d3', 'content - message from username2 to username1 (chat1)');
--- message from username3 to no user (general message)
-INSERT INTO chat_message (chat_id, from_chat_user_id, content) VALUES('f729dd6c-a130-4725-9d98-10137727a821', '791be344-1acb-4d15-9284-fe325edfb481', 'content - message from username3 to the chat (no specific user) (chat1)');
+-- message from username3 to no user (general message), so fill as TO SELF
+INSERT INTO chat_message (chat_id, from_chat_user_id, to_chat_user_id, content) VALUES('f729dd6c-a130-4725-9d98-10137727a821', '791be344-1acb-4d15-9284-fe325edfb481', '791be344-1acb-4d15-9284-fe325edfb481', 'content - message from username3 to the chat (no specific user) (chat1)');
 
 -- Chat2 Data
 INSERT INTO chat (id, name) VALUES ('b863b0ba-44ff-49d4-a3a8-d62b59cb90fd', 'chat2');
@@ -35,21 +37,21 @@ INSERT INTO chat_user (id, account_id, chat_id) VALUES ('eac92561-0666-47dc-9cac
 INSERT INTO chat_message (chat_id, from_chat_user_id, to_chat_user_id, content) VALUES('b863b0ba-44ff-49d4-a3a8-d62b59cb90fd', '477519de-f226-42a1-bd76-750f02f025fe', '739e2036-18ab-48a6-b4c3-1fa506e08190', 'content - message from username1 to username2 (chat2)');
 -- message from username2 to username1
 INSERT INTO chat_message (chat_id, from_chat_user_id, to_chat_user_id, content) VALUES('b863b0ba-44ff-49d4-a3a8-d62b59cb90fd', '739e2036-18ab-48a6-b4c3-1fa506e08190', '477519de-f226-42a1-bd76-750f02f025fe', 'content - message from username2 to username1 (chat2)');
--- message from username3 to no user (general message)
-INSERT INTO chat_message (chat_id, from_chat_user_id, content) VALUES('b863b0ba-44ff-49d4-a3a8-d62b59cb90fd', 'eac92561-0666-47dc-9cac-f7268edf088c', 'content - message from username3 to the chat (no specific user) (chat2)');
+-- message from username3 to no user (general message), so fill as TO SELF
+INSERT INTO chat_message (chat_id, from_chat_user_id, to_chat_user_id, content) VALUES('b863b0ba-44ff-49d4-a3a8-d62b59cb90fd', 'eac92561-0666-47dc-9cac-f7268edf088c', 'eac92561-0666-47dc-9cac-f7268edf088c', 'content - message from username3 to self/the-chat (chat2)');
+-- message from username3 to no user (general message), so fill as TO SELF
+INSERT INTO chat_message (chat_id, from_chat_user_id, to_chat_user_id, content) VALUES('b863b0ba-44ff-49d4-a3a8-d62b59cb90fd', 'eac92561-0666-47dc-9cac-f7268edf088c', 'eac92561-0666-47dc-9cac-f7268edf088c', '(again) content - message from username3 to self/the-chat (chat2)');
 
--- Get messages from Chat1
-SELECT * FROM chat_message WHERE chat_id = 'f729dd6c-a130-4725-9d98-10137727a821';
--- Get users from Chat1
-SELECT * FROM chat_user WHERE chat_id = 'f729dd6c-a130-4725-9d98-10137727a821';
--- Get messages from Chat2
-SELECT * FROM chat_message WHERE chat_id = 'b863b0ba-44ff-49d4-a3a8-d62b59cb90fd';
--- Get users from Chat1
-SELECT * FROM chat_user WHERE chat_id = 'b863b0ba-44ff-49d4-a3a8-d62b59cb90fd';
 
--- >>>>>>>>>>>>>>>>>>>>>>
--- TODO
 --
--- Unions or someting? Example getting users authenticated (online)
--- Example union getting Chat messages with all "branching" data
--- >>>>>>>>>>>>>>>>>>>>>>
+-- Get some data
+--
+
+-- Roster & Transcript from Chat1
+SELECT * FROM roster WHERE chat_id = 'f729dd6c-a130-4725-9d98-10137727a821';
+SELECT * FROM transcript WHERE chat_id = 'f729dd6c-a130-4725-9d98-10137727a821'; -- returns 3 rows
+
+-- Roster & Transcript from Chat2
+SELECT * FROM roster WHERE chat_id = 'b863b0ba-44ff-49d4-a3a8-d62b59cb90fd';
+SELECT * FROM transcript WHERE chat_id = 'b863b0ba-44ff-49d4-a3a8-d62b59cb90fd'; -- returns 4 rows
+
