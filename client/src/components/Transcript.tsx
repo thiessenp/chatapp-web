d8ad1b91f11e893d3cbe099bde695ecf5455215f
Peter <thiessenp@gmail.com>
2021-02-08 15:54:55 -0500

    Adds Client Transcrit keynav

--- client/src/components/Transcript.tsx
+++ client/src/components/Transcript.tsx
@@ -1,4 +1,31 @@
+import {ListNav} from 'keynav-web';
+import {useRef, useEffect} from 'react';
+
+
 export function Transcript(props) {
+    const transcriptRef = useRef<HTMLOListElement>(null);
+ 
+    // let listNav;
+    // console.log('props.transcript', props.transcript)
+    // console.log('transcriptRef', transcriptRef, transcriptRef.current && transcriptRef.current.children.length);
+
+    useEffect(() => {
+        // TODO:
+        // - Check if this is a good way to add/rem behavior on dynamic lists?
+        if (transcriptRef.current && transcriptRef.current.children.length) {
+            const listNav = new ListNav({
+                listEl: transcriptRef.current,
+                listItemsSelector: 'li'
+            });
+            console.log('add behavior', listNav, transcriptRef.current.children.length);
+
+            return () => {
+                console.log('rem behavior')
+                listNav.removeBehavior();
+            }
+        }
+    }, [props]);
+
     return (
         <section>
             <h3>Transcript</h3>
@@ -7,7 +34,7 @@ export function Transcript(props) {
             }
 
             {(props.transcript.length > 0 && 
-                <ol>
+                <ol ref={transcriptRef}>
                 {(
                     props.transcript.map(message => {
                         return (
